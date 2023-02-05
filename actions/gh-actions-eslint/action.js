6d551a7a4253ea3028a6b3e38f6582f94c9fd697
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2023-02-05 13:44:06 +0100

    Add GitHub actions ESLint action

---
+++ actions/gh-actions-eslint/action.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const ghaCore = require('@actions/core');
+const { exec } = require('@actions/exec');
+
+const formatterPath = path.join(__dirname, 'formatter.js');
+
+const {
+  getInput,
+  getMultilineInput,
+  getNpmExecArguments,
+} = require('@thnetii/gh-actions-core-helpers');
+
+const toolArgs = getMultilineInput('arguments');
+const execCwd = getInput('working-directory');
+const npmExecArgs = getNpmExecArguments();
+npmExecArgs.push('--', 'eslint', '--format', formatterPath, ...toolArgs);
+
+exec('npm', npmExecArgs, { cwd: execCwd, ignoreReturnCode: true }).then(
+  (exitCode) => {
+    ghaCore.setOutput('eslint-exitcode', exitCode);
+    process.exitCode = exitCode;
+  }
+);
