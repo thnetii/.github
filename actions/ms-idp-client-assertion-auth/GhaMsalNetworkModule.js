d11bbb53da7440c3a20810536494a3ddc623c0bb
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2023-04-23 16:14:44 +0200

    MSAL: fix GitHub actions POST request function

--- actions/ms-idp-client-assertion-auth/GhaMsalNetworkModule.js
+++ actions/ms-idp-client-assertion-auth/GhaMsalNetworkModule.js
@@ -3,28 +3,16 @@ const { HttpClient, HttpClientError } = require('@actions/http-client');
 /** @typedef {import('@azure/msal-common').INetworkModule} INetworkModule */
 
 /**
- * @param {import('@actions/http-client/lib/interfaces').TypedResponse<T>} httpResp
- * @returns {import('@azure/msal-common').NetworkResponse<T>}
- * @template T
+ * @param {import('@actions/http-client').HttpClientResponse['message']['headers']} headers
  */
-function toNetworkResponse(httpResp) {
-  if (httpResp.result === null)
-    throw new HttpClientError(
-      'Deserialized JSON response response payload is null.',
-      httpResp.statusCode
-    );
-  const headers = Object.fromEntries(
-    Object.entries(httpResp.headers).map(([header, value]) => {
+function simplifyHeaders(headers) {
+  return Object.fromEntries(
+    Object.entries(headers).map(([header, value]) => {
       if (typeof value === 'string') return [header, value];
       if (Array.isArray(value)) return [header, value.join('\n')];
       return [header, ''];
     })
   );
-  return {
-    status: httpResp.statusCode,
-    body: httpResp.result,
-    headers,
-  };
 }
 
 /** @implements INetworkModule */
@@ -41,22 +29,31 @@ class GhaMsalNetworkModule {
   async sendGetRequestAsync(url, options) {
     /** @type {import('@actions/http-client/lib/interfaces').TypedResponse<T>} */
     const resp = await this.httpClient.getJson(url, options?.headers);
-    return toNetworkResponse(resp);
+    if (resp.result === null)
+      throw new HttpClientError('HTTP result object is null', resp.statusCode);
+    return {
+      status: resp.statusCode,
+      body: resp.result,
+      headers: simplifyHeaders(resp.headers),
+    };
   }
 
   /**
-   * @param {string} url
-   * @param {import('@azure/msal-common').NetworkRequestOptions | undefined} [options]
-   * @template T
+   * @type {INetworkModule['sendPostRequestAsync']}
    */
   async sendPostRequestAsync(url, options) {
-    /** @type {import('@actions/http-client/lib/interfaces').TypedResponse<T>} */
-    const resp = await this.httpClient.postJson(
+    const resp = await this.httpClient.post(
       url,
-      options?.body,
+      options?.body || '',
       options?.headers
     );
-    return toNetworkResponse(resp);
+    const msg = resp.message;
+    const bodyString = await resp.readBody();
+    return {
+      status: msg.statusCode || 200,
+      body: JSON.parse(bodyString),
+      headers: simplifyHeaders(msg.headers),
+    };
   }
 }
 
