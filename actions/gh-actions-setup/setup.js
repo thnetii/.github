285f5b80c27119e95e84fae8ff9f4280b4d8a878
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2023-05-21 21:30:09 +0000

    Bootstrapper run clean-install, but checks if all package dependencies are missing first

--- actions/gh-actions-setup/setup.js
+++ actions/gh-actions-setup/setup.js
@@ -1,19 +1,33 @@
-const { execSync } = require('child_process');
-const path = require('path');
+/* eslint-disable no-console */
+const { createRequire } = require('node:module');
+const { execSync } = require('node:child_process');
+const path = require('node:path');
 
 /** @param {string} actionPath */
-module.exports = (actionPath) => {
+module.exports = async (actionPath) => {
+  const actionRequire = createRequire(actionPath);
+  const actionPackageJsonPath = path.join(actionPath, 'package.json');
+  const { name, dependencies = {} } = actionRequire(actionPackageJsonPath);
+  if (typeof dependencies !== 'object')
+    throw new TypeError(
+      'Expected package dependencies to be a JSON object value'
+    );
   const repoRootPath = path.resolve(path.join(__dirname, '..', '..'));
-  const workspacePath = path.relative(repoRootPath, actionPath);
-  execSync(`node -v`, {
-    cwd: repoRootPath,
-    stdio: [process.stdin, process.stdout, process.stderr],
-  });
-  execSync(`npm -v`, {
-    cwd: repoRootPath,
-    stdio: [process.stdin, process.stdout, process.stderr],
-  });
-  execSync(`npm install --workspace ${workspacePath} --omit=dev`, {
+  let unfulfilledDependencies = false;
+  try {
+    for (const dependency of Object.keys(dependencies)) {
+      actionRequire(dependency);
+    }
+  } catch {
+    unfulfilledDependencies = true;
+  }
+  if (!unfulfilledDependencies) {
+    console.log(`All dependencies of package ${name} are installed.`);
+    return;
+  }
+
+  console.log(`Executing npm to install dependencies.`);
+  execSync(`npm clean-install --workspaces --omit=dev`, {
     cwd: repoRootPath,
     stdio: [process.stdin, process.stdout, process.stderr],
   });
