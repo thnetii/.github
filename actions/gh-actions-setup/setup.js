d872d38e9be821d06244e7dd2c83a98de7ef23a5
Fredrik Høisæther Rasch <fredrik.rasch@arva.no>
2023-05-22 10:58:51 +0000

    Revert `npm install --workspaces`
    Instead use `npm install --workspace <name>`

--- actions/gh-actions-setup/setup.js
+++ actions/gh-actions-setup/setup.js
@@ -7,27 +7,10 @@ const path = require('node:path');
 module.exports = async (actionPath) => {
   const actionRequire = createRequire(actionPath);
   const actionPackageJsonPath = path.join(actionPath, 'package.json');
-  const { name, dependencies = {} } = actionRequire(actionPackageJsonPath);
-  if (typeof dependencies !== 'object')
-    throw new TypeError(
-      'Expected package dependencies to be a JSON object value'
-    );
+  const { name } = actionRequire(actionPackageJsonPath);
   const repoRootPath = path.resolve(path.join(__dirname, '..', '..'));
-  let unfulfilledDependencies = false;
-  try {
-    for (const dependency of Object.keys(dependencies)) {
-      actionRequire(dependency);
-    }
-  } catch {
-    unfulfilledDependencies = true;
-  }
-  if (!unfulfilledDependencies) {
-    console.log(`All dependencies of package ${name} are installed.`);
-    return;
-  }
-
   console.log(`Executing npm to install dependencies.`);
-  execSync(`npm clean-install --workspaces --omit=dev`, {
+  execSync(`npm install --workspace "${name}" --omit=dev`, {
     cwd: repoRootPath,
     stdio: [process.stdin, process.stdout, process.stderr],
   });
