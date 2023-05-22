4c7e1266ef514a770fcfeb5ded72a43531c02181
Fredrik Høisæther Rasch <fredrik.rasch@arva.no>
2023-05-22 12:12:31 +0000

    Simplify bootstrapper, install dependency local to bootstrapped package

--- actions/gh-actions-setup/setup.js
+++ actions/gh-actions-setup/setup.js
@@ -1,19 +1,12 @@
 /* eslint-disable no-console */
-const { createRequire } = require('node:module');
 const { execSync } = require('node:child_process');
-const path = require('node:path');
 
 /** @param {string} actionPath */
 module.exports = async (actionPath) => {
-  const actionRequire = createRequire(actionPath);
-  const actionPackageJsonPath = path.join(actionPath, 'package.json');
-  const { name } = actionRequire(actionPackageJsonPath);
-  const repoRootPath = path.resolve(path.join(__dirname, '..', '..'));
-  console.log(`Executing npm to install dependencies.`);
-  const command = `npm install --workspace "${name}" --omit=dev`;
+  const command = `npm install --prefix "${actionPath}" --omit=dev`;
   console.log(`[command]${command}`);
   execSync(command, {
-    cwd: repoRootPath,
+    cwd: actionPath,
     stdio: [process.stdin, process.stdout, process.stderr],
   });
 };
