6c1f6d2470d8c7bdc4d9c112ea7322b20752de36
Fredrik Høisæther Rasch <fredrik.rasch@arva.no>
2023-05-22 12:18:58 +0000

    Add additional options to bootstrapper: no-audit, no-fund, install-links

--- actions/gh-actions-setup/setup.js
+++ actions/gh-actions-setup/setup.js
@@ -3,7 +3,7 @@ const { execSync } = require('node:child_process');
 
 /** @param {string} actionPath */
 module.exports = async (actionPath) => {
-  const command = `npm install --prefix "${actionPath}" --omit=dev`;
+  const command = `npm install --prefix "${actionPath}" --omit=dev --no-audit --no-fund --install-links`;
   console.log(`[command]${command}`);
   execSync(command, {
     cwd: actionPath,
