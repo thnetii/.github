606b70c8deafeeb6d9e8e522fd52771d54da2871
Fredrik Høisæther Rasch <fredrik.rasch@arva.no>
2023-05-23 07:52:31 +0000

    Update submodules for `--install-links` to work, even when not needed

--- actions/gh-actions-setup/setup.js
+++ actions/gh-actions-setup/setup.js
@@ -1,12 +1,21 @@
 /* eslint-disable no-console */
 const { execSync } = require('node:child_process');
+const path = require('node:path');
 
 /** @param {string} actionPath */
 module.exports = async (actionPath) => {
-  const command = `npm install --prefix "${actionPath}" --omit=dev --no-audit --no-fund --install-links`;
-  console.log(`[command]${command}`);
-  execSync(command, {
-    cwd: actionPath,
+  const repoRootPath = path.resolve(path.join(__dirname, '..', '..'));
+  const workspacePath = path.relative(repoRootPath, actionPath);
+  const gitCommand = `git submodule update --init`;
+  console.log(`[command]${gitCommand}`);
+  execSync(gitCommand, {
+    cwd: repoRootPath,
+    stdio: [process.stdin, process.stdout, process.stderr],
+  });
+  const npmCommand = `npm install --workspace "${workspacePath}" --no-audit --no-fund --install-links`;
+  console.log(`[command]${npmCommand}`);
+  execSync(npmCommand, {
+    cwd: repoRootPath,
     stdio: [process.stdin, process.stdout, process.stderr],
   });
 };
